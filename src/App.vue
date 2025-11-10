576c904a1c3fafe4d024894df0a9a5b1c2efd83e
thaivm-0609 <vuongthai95@gmail.com>
2025-11-10 16:26:15 +0700

    feat: ref and reactive

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,80 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+import {ref, reactive} from 'vue';
+
+//ref: dùng với kiểu dữ liệu nguyên thủy: number, string, boolean
+//ref sẽ tạo biến là 1 object, bên trong có thuộc tính .value=giá trị của biến
+//khi sử dụng giá trị của biến để tính toán => tenBien.value
+const count = ref(10); //khởi tạo biến count gán giá trị bằng 0
+const name = ref('thaivm2'); //string
+const isActive = ref(true); //boolean
+
+const countClick = () => {
+  console.log(count);
+  count.value++;
+}
+
+//reactive: dùng với object, array
+const thaivm2 = reactive({ //object
+  name: "Vương Minh Thái",
+  age: 100,
+  isActive: true,
+})
+
+const listUsers = reactive([ //array
+  {
+    name: "thaivm2",
+    age: 100,
+    isActive: true,
+  },
+  {
+    name: "thaivm3",
+    age: 1,
+    isActive: false,
+  }
+]);
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- ref -->
+    <button class="btn btn-success" @click="countClick">Test</button>
+    <p>{{ count }}</p> <br>
+    <span>{{ name }}</span> <br>
+    <span>{{ isActive }}</span>
 
-    <div class="wrapper">
+    <!-- reactive: tenObject.key --> 
+    <p>{{ thaivm2.name }}</p>
+    <p>{{ thaivm2.age }}</p>
+    <p>{{ thaivm2.isActive }}</p>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ listUsers[0].name }}</td>
+          <td>{{ listUsers[0].age }}</td>
+          <td>{{ listUsers[0].isActive }}</td>
+        </tr>
+        <tr>
+          <td>{{ listUsers[1].name }}</td>
+          <td>{{ listUsers[1].age }}</td>
+          <td>{{ listUsers[1].isActive }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div> -->
   </header>
 
   <main>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 createApp(App).mount('#app')
