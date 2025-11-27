2eabbddc66a449a07ca3a647c667bd8aa6121490
thaivm-0609 <vuongthai95@gmail.com>
2025-11-27 09:54:28 +0700

    feat: component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+//component con
+//khai báo props được truyền từ component cha sang
+const props = defineProps({
+    p: Object,
+})
+
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+        <img :src="p.image" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ p.name }}</h5>
+            <p v-if="p.price" class="card-text">Price: {{ p.price }}</p>
+            <p v-if="p.level" class="card-text">Level: {{ p.level }}</p>
+            <p v-if="p.description" class="card-text">Description: {{ p.description }}</p>
+            <a href="#" class="btn btn-primary">Buy now</a>
+        </div>
+    </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,71 @@
 <script setup>
+//component cha
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import ProductCard from '@/components/ProductCard.vue'; //phải import component con thì mới sử dụng được
 
+const products = ref([]); //dữ liệu cho sản phẩm mới
+const bestSellers = ref([]); //dữ liệu cho sản phẩm bán chạy
+const users = ref([]);
+const getProducts = async () => {
+    const response = await axios.get('http://localhost:3000/products');
+    products.value = response.data;
+    bestSellers.value = response.data;
+}
+
+const getUsers = async () => {
+    const response = await axios.get('http://localhost:3000/users');
+    users.value = response.data;
+}
+
+onMounted(() => {
+    getProducts();
+    getUsers();
+})
 </script>
 
 <template>
     <h1>Đây là trang chủ</h1>
+
+    <h2>Sản phẩm mới</h2>
+    <div class="d-flex">
+        <!-- chạy vòng lặp cho biến products -->
+        <!-- Không sử dụng component -->
+        <!-- <div v-for="p in products" class="card" style="width: 18rem;">
+            <img :src="p.image" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ p.name }}</h5>
+                <p class="card-text">Price: {{ p.price }}</p>
+                <p class="card-text">Description: {{ p.description }}</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+            </div>
+        </div> -->
+
+        <!-- Sử dụng component ProductCard
+        sử dụng props (thuộc tính) để truyền dữ liệu từ 
+        component cha sang component con
+        :key="value"
+        -->
+        <ProductCard v-for="product in products" :p="product"/>
+    </div>
+
+    <h2>Sản phẩm bán bán chạy</h2>
+    <div class="d-flex">
+        <!-- chạy vòng lặp cho biến products -->
+        <!-- <div v-for="p in bestSellers" class="card" style="width: 18rem;">
+            <img :src="p.image" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ p.name }}</h5>
+                <p class="card-text">Price: {{ p.price }}</p>
+                <p class="card-text">Description: {{ p.description }}</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+            </div>
+        </div> -->
+        <ProductCard v-for="product in bestSellers" :p="product"/>
+    </div>
+
+    <h2>Danh sách người dùng</h2>
+    <div class="d-flex">
+        <ProductCard v-for="user in users" :p="user"/>
+    </div>
 </template>
